758d465b01ae0dde4e3ab9b1f0de05aaac7f944e
RuoChen95 <xrc_1995@163.com>
2017-03-06 22:47:36 +0800

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,20 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("pink")
+
+    brad = turtle.Turtle() #int
+    brad.forwad(100)
+    brad.right(90)
+    brad.forwad(100)
+    brad.right(90)
+    brad.forwad(100)
+    brad.right(90)
+    brad.forwad(100)
+    brad.right(90)
+
+
+    window.exitonclick()
+
+draw_square()
